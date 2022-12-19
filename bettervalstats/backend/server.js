46640c6738e3768bfb03a84d58b8e4700eaab0ff
caleb1027 <caleb.li1027@gmail.com>
2022-12-18 20:13:41 -0500

    Initial Commit, created server and connected DB

---
+++ bettervalstats/backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Connected to MongoDB");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+});
